d87d1127765c38f4ad73956a178adb955de70115
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-05-29 20:49:22 -0400

    Adding xhr_request? helper

--- app/controllers/clockface/application_controller.rb
+++ app/controllers/clockface/application_controller.rb
@@ -1,5 +1,11 @@
 module Clockface
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    private
+
+    def xhr_request?
+      (defined? request) && request.xhr?
+    end
   end
 end
