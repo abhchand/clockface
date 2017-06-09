57558e37cdb92e54c5bbf09054e2ca5987bd2fe8
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-06-08 22:39:36 -0400

    Adding DashboardController specs

---
+++ spec/controllers/clockface/dashboard_controller_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+module Clockface
+  RSpec.describe DashboardController, type: :controller do
+    routes { Clockface::Engine.routes }
+
+    describe "GET #show" do
+      it "returns 200 and renders the dashboard/show template" do
+        get :show
+
+        expect(response.status).to eq(200)
+        expect(response).to render_template("dashboard/show")
+      end
+
+      it "assigns the list of all scheduled jobs" do
+        job1 = create(:clockwork_scheduled_job)
+        job2 = create(:clockwork_scheduled_job)
+
+        get :show
+
+        expect(assigns(:scheduled_jobs)).to match_array([job1, job2])
+      end
+    end
+  end
+end
