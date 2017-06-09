bd6e89c2f1df861e5015a8db06db92da1479d28e
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-06-08 22:41:23 -0400

    Adding ScheduledJobsController specs

---
+++ spec/controllers/clockface/scheduled_jobs_controller_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+module Clockface
+  RSpec.describe ScheduledJobsController, type: :controller do
+    routes { Clockface::Engine.routes }
+
+    describe "GET #show" do
+    end
+  end
+end
