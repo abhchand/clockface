dad136c474f9cce50f970754d86f95b69e315d6d
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-06-11 22:53:49 -0400

    Adding JobsPresenter and initializing I18n file

---
+++ app/presenters/clockface/jobs_presenter.rb
@@ -0,0 +1,31 @@
+# TODO: Needs specs
+module Clockface
+  class JobsPresenter < SimpleDelegator
+    def period
+      I18n.t(
+        "datetime.distance_in_words.x_#{job.period_units}",
+        count: job.period_value
+      )
+    end
+
+    def at
+      # TODO: This needs to be properly translatable via I18n
+      job.at
+    end
+
+    def if_condition
+      if job.if_condition.present?
+        I18n.t(
+          "activerecord.attributes."\
+            "clockface/clockwork_scheduled_job.if_condition.#{job.if_condition}"
+        )
+      end
+    end
+
+    private
+
+    def job
+      __getobj__
+    end
+  end
+end
