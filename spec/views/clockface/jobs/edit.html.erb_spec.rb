f0e116bb7e1ee0d15fa57c55c0800427e2f29728
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-06-17 01:44:48 -0400

    Adding link back to jobs/index from jobs/new and jobs/edit

--- spec/views/clockface/jobs/edit.html.erb_spec.rb
+++ spec/views/clockface/jobs/edit.html.erb_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 module Clockface
   RSpec.describe "clockface/jobs/edit.html.erb", type: :view do
+    let(:job) { create(:clockwork_scheduled_job) }
+
+    before(:each) { assign(:job, job) }
+
     it "renders the flash" do
       render
       expect(view).to render_template(partial: "_flash")
@@ -10,6 +14,15 @@ module Clockface
     it "renders the heading" do
       render
       expect(page).to have_content(t("clockface.jobs.edit.heading"))
+    end
+
+    it "renders the back link" do
+      render
+      link = page.
+        find(".jobs-edit__heading-banner .jobs-edit__heading-banner-link")
+
+      expect(link["href"]).to eq(clockface.jobs_path)
+      expect(link).to have_selector(".glyphicon-chevron-left")
     end
 
     it "renders the jobs form" do
--- spec/views/clockface/jobs/new.html.erb_spec.rb
+++ spec/views/clockface/jobs/new.html.erb_spec.rb
@@ -12,6 +12,15 @@ module Clockface
       expect(page).to have_content(t("clockface.jobs.new.heading"))
     end
 
+    it "renders the back link" do
+      render
+      link = page.
+        find(".jobs-new__heading-banner .jobs-new__heading-banner-link")
+
+      expect(link["href"]).to eq(clockface.jobs_path)
+      expect(link).to have_selector(".glyphicon-chevron-left")
+    end
+
     it "renders the jobs form" do
       render
       expect(view).to render_template(partial: "_job_form")
