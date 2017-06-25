83e708c917c069da6c1f66b9eb9ec8232e203fbb
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-06-25 15:53:49 -0400

    Logging with a dedicated logger

--- spec/dummy/app/workers/example_worker.rb
+++ spec/dummy/app/workers/example_worker.rb
@@ -2,6 +2,12 @@ class ExampleWorker
   include Sidekiq::Worker
 
   def perform(id)
-    Sidekiq.logger.info "Running Example Worker with id: #{id}"
+    # Log to a dedicated log file to easily isolate messages while testing
+    logger = Logger.new(Rails.root.join("log", "example_worker.log"))
+
+    message = "Running Example Worker with id: #{id}"
+
+    Sidekiq.logger.info(message)
+    logger.info(message)
   end
 end
