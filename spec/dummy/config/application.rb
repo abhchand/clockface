2bb7ff5d8ce509da43dbf76317a88b93bbfed826
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-06-25 15:59:23 -0400

    Reverting dummy host timezone to UTC. Keep it simple

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -26,7 +26,7 @@ module Dummy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.time_zone = "Hawaii"
+    config.time_zone = "UTC"
   end
 end
 
