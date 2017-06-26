423aa0fe8cd9242c84e41c6c7f2742916e46e2db
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-06-25 20:37:05 -0400

    Adding Clockface wrapper for '#sync_database_events'

--- lib/clockface.rb
+++ lib/clockface.rb
@@ -1,5 +1,29 @@
+require "clockwork"
+require "clockwork/database_events"
+
 require "clockface/engine"
 
 module Clockface
-  # Your code goes here...
+  module Methods
+    def sync_database_events(opts = {}, &block)
+      ::Clockwork.manager = ::Clockwork::DatabaseEvents::Manager.new
+      logger = Clockface::Engine.config.clockface.logger
+
+      unless block_given?
+        raise "Please specify a block to Clockface::sync_database_events"
+      end
+
+      ::Clockwork::sync_database_events(model: Clockface::ClockworkScheduledJob, every: opts[:every]) do |job|
+        if job.enabled?
+          logger.info "[Clockface] Running Job #{job.id} (#{job.name})"
+          yield(job)
+          job.update(last_run_at: Time.zone.now)
+        else
+          logger.info "[Clockface] Skipping Disabled Job #{job.id} (#{job.name})"
+        end
+      end
+    end
+  end
+
+  extend Methods
 end
