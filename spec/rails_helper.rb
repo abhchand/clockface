952868664f4eb011d6025a7542a6f88cf2573bd2
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-12-23 18:00:04 -0500

    Maintain the test schema migrations automatically

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,3 +11,6 @@ require "rspec/rails"
 # Only require the top-level files that are one level deep
 # All support/** sub-folders should be required by the top-level files
 Dir[Rails.root.join("spec/support/*.rb")].each { |file| require file }
+
+# Maintain the test schema to match the development schema at all times
+ActiveRecord::Migration.maintain_test_schema!
