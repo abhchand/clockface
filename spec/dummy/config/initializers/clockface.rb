b3a02a628ea36a46bf6449981d6619ae19756116
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-12-25 12:38:51 -0500

    Removing useless/cluttering comment

--- spec/dummy/config/initializers/clockface.rb
+++ spec/dummy/config/initializers/clockface.rb
@@ -2,7 +2,4 @@ Clockface::Engine.configure do |app|
   app.config.clockface.time_zone = "Pacific Time (US & Canada)"
   app.config.clockface.logger = Rails.logger
   app.config.clockface.tenant_list = ALL_TENANTS
-  # Let these default since we use the apartment gem
-  # app.config.clockface.current_tenant_proc =
-  # app.config.clockface.execute_in_tenant_proc =
 end
