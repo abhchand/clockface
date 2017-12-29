86407d9b64419d08dfead7b06b977357de0610e1
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-12-28 23:38:07 -0500

    Add Capybara Helper for selecting options

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,6 +1,7 @@
 require_relative "translation_helpers"
 require_relative "tenancy_helpers"
 require_relative "subdomain_helpers"
+require_relative "capybara_helpers"
 
 %w(views controllers).each do |subdirectory|
   Dir[Rails.root.join("spec/support/#{subdirectory}/*.rb")].each do |file|
@@ -17,4 +18,5 @@ RSpec.configure do |config|
   config.include Clockface::Engine.routes.url_helpers, type: [:view]
 
   config.include SubdomainHelpers, type: :feature
+  config.include CapybaraHelpers, type: :feature
 end
