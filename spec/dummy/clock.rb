5f9bdfd2a24f1fa512f7e7d23463d1997d7b62cb
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-12-29 22:03:50 -0500

    Use dot syntax instead of nested module syntax

--- spec/dummy/clock.rb
+++ spec/dummy/clock.rb
@@ -3,18 +3,16 @@ require_relative "./config/environment"
 
 require "clockface"
 
-module Clockface
-  sync_database_events(every: 10.seconds) do |job|
-    # Each app can do anything they want here with the yielded job record
-    # For this dummy app, we choose to store a hash in the `command` field
-    # which we first parse and then use to construct a Sidekiq `perform_sync`
-    # statement. This could be adapated to fit any background schedule, like
-    # ActiveJob, Rufus, etc..
+Clockface.sync_database_events(every: 10.seconds) do |job|
+  # Each app can do anything they want here with the yielded job record
+  # For this dummy app, we choose to store a hash in the `command` field
+  # which we first parse and then use to construct a Sidekiq `perform_sync`
+  # statement. This could be adapated to fit any background schedule, like
+  # ActiveJob, Rufus, etc..
 
-    cmd_hash = JSON.parse(job.command)
-    klass = cmd_hash["class"]
+  cmd_hash = JSON.parse(job.command)
+  klass = cmd_hash["class"]
 
-    logger = Logger.new(Rails.root.join("log", "example_workers.log"))
-    klass.constantize.perform_async
-  end
+  logger = Logger.new(Rails.root.join("log", "example_workers.log"))
+  klass.constantize.perform_async
 end
