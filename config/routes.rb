360893ce8294ac19bf3ee516951c5be8831381b5
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2018-01-04 00:44:49 -0500

    Explanatory comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Clockface::Engine.routes.draw do
   root to: redirect("/clockface/events")
   resources :events, except: [:show] do
+    # Rails routes don't include a visible deletion page, only a `#destroy`
+    # endpoint. Define a custom one
     get :delete, to: "events#delete"
   end
 end
