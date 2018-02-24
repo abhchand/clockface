1e8b3838ab9910aaf18b20b7ba669f11b8cc2c5c
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2018-02-24 14:56:43 -0500

    Typo

--- spec/dummy/clock.rb
+++ spec/dummy/clock.rb
@@ -6,7 +6,7 @@ require "clockface"
 Clockface.sync_database_events(every: 10.seconds) do |event|
   # Each app can do anything they want here with the yielded event record
   # For this dummy app, we choose to store a hash in the `command` field
-  # which we first parse and then use to construct a Sidekiq `perform_sync`
+  # which we first parse and then use to construct a Sidekiq `perform_async`
   # statement. This could be adapated to fit any background schedule, like
   # ActiveJob, Rufus, etc..
 
