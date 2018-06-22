a5f0056b28d2efab9fc47bf36fdb3bb55f504e67
Abhishek Chandrasekhar <me@abhchand.me>
2018-06-22 16:09:37 -0400

    Fixing `expand_path` calls for `Style/ExpandPathArguments` errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../dummy/config/environment", __FILE__)
+require File.expand_path("dummy/config/environment", __dir__)
 
 if Rails.env.production?
   abort("The Rails environment is running in production mode!")
